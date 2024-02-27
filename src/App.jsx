4f4d78f1de44eb9523e5c0d9fa727f5cb288b3c1
snelson23 <94838190+snelson23@users.noreply.github.com>
2024-02-27 14:01:22 -0500

    images not pulling through

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./styles.css";
 import Wheel from "./wheel";
 import AddNames from "./addNames";
 import { useState } from "react";
+import wonw from "./images/wonw.png";
+import williamslogo from "./images/williamslogo.png";
 
 export default function App() {
   const [nameList, setNameList] = useState(["Name 1", "Name 2", "Name 3", "Name 4"]);
@@ -14,14 +16,14 @@ export default function App() {
   return (
     <div className="App">
       <div className="title">
-        <img src="src/images/wonw.png" alt="wonw" />
+        <img src={wonw} alt="wonw" />
       </div>
       <div className="main">
         <Wheel key={nameList.length} nameList={nameList} />
         <AddNames nameList={nameList} setNameList={addName} show={showNames} />
       </div>
       <div className="brand">
-        <img src="src/images/williamslogo.png" alt="logo" />
+        <img src={williamslogo} alt="logo" />
       </div>
       <p className="show-text" onClick={() => setShowNames(!showNames)}>{showNames ? 'Hide' : 'Edit Names'}</p>
     </div>
